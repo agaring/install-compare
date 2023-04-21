903495acaf2030a0b201475f988a8bec6ce44624
Arnaud GARING <arnaud.garing67@gmail.com>
2023-04-21 09:24:05 +0200

    Create main file
    This file will be used as the new entry point into install-compare

---
+++ main.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    print('Main')
+
+    args = parse_args()
